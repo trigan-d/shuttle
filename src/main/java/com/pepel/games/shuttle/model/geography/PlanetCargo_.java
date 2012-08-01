c6d3fb0c1eeed2e8b29ccbf8fd87ac2a8eaf549b
trigan-d <trigan.sda@gmail.com>
2012-08-01 22:45:09 +0700

    test commit 3

--- src/main/java/com/pepel/games/shuttle/model/geography/PlanetCargo_.java
+++ src/main/java/com/pepel/games/shuttle/model/geography/PlanetCargo_.java
@@ -7,14 +7,14 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2012-08-01T21:52:37.298+0700")
+@Generated(value="Dali", date="2012-08-01T22:34:00.868+0700")
 @StaticMetamodel(PlanetCargo.class)
 public class PlanetCargo_ {
 	public static volatile SingularAttribute<PlanetCargo, Long> id;
+	public static volatile SingularAttribute<PlanetCargo, Planet> planet;
 	public static volatile SingularAttribute<PlanetCargo, Direction> direction;
 	public static volatile SingularAttribute<PlanetCargo, Cargo> cargo;
 	public static volatile SingularAttribute<PlanetCargo, Integer> perDay;
 	public static volatile SingularAttribute<PlanetCargo, Date> lastChange;
 	public static volatile SingularAttribute<PlanetCargo, Integer> lastAmount;
-	public static volatile SingularAttribute<PlanetCargo, Planet> planet;
 }
--- src/main/java/com/pepel/games/shuttle/model/geography/Province_.java
+++ src/main/java/com/pepel/games/shuttle/model/geography/Province_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2012-08-01T21:54:18.339+0700")
+@Generated(value="Dali", date="2012-08-01T22:34:12.543+0700")
 @StaticMetamodel(Province.class)
 public class Province_ {
 	public static volatile SingularAttribute<Province, Long> id;
@@ -14,8 +14,8 @@ public class Province_ {
 	public static volatile SingularAttribute<Province, Integer> x;
 	public static volatile SingularAttribute<Province, Integer> y;
 	public static volatile SingularAttribute<Province, Long> zone;
+	public static volatile ListAttribute<Province, Planet> planets;
 	public static volatile SingularAttribute<Province, Boolean> occupied;
 	public static volatile SingularAttribute<Province, Cargo> deficit;
 	public static volatile SingularAttribute<Province, Cargo> proficit;
-	public static volatile ListAttribute<Province, Planet> planets;
 }
